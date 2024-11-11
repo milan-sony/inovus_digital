2d21b6f1ce64df387dcb0ff549f8be35759a31e0
Milan <milansonyofficial@gmail.com>
2024-11-11 11:40:01 +0530

    favicon added on index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div>
-      <h1>Inovus Digital</h1>
-    </div>
-  )
+	return (
+		<div>
+			<h1>Inovus Digital</h1>
+		</div>
+	)
 }
 
 export default App
