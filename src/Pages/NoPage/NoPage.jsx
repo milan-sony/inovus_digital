06a6c0eab80136fb53bd838e100000c4011d099a
Milan <milansonyofficial@gmail.com>
2024-11-12 10:18:31 +0530

    feat: 404 page created

---
+++ src/Pages/NoPage/NoPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NoPage.css'
+import { Link } from 'react-router-dom'
+
+function NoPage() {
+    return (
+        <div className='page-container'>
+            <div className='h-screen flex flex-col justify-center items-center'>
+                <h1 className='text-9xl text-white font-Open-Sans font-black pb-3'>404</h1>
+                <h3 className='text-5xl text-white font-Open-Sans font-semibold capitalize pb-3'>Page not found !</h3>
+                <Link to={"/"} className='text-white text-5xl font-black'><i class="fa-solid fa-left-long"></i></Link>
+            </div>
+        </div>
+    )
+}
+
+export default NoPage
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home/Home'
 import User from '../Pages/User/User'
+import NoPage from '../Pages/NoPage/NoPage'
 
 
 function Router() {
@@ -9,6 +10,7 @@ function Router() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='user' element={<User />} />
+            <Route path="*" element={<NoPage />} />
         </Routes>
     )
 }
