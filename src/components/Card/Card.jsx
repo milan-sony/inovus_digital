382764a521f1985bc59f91c5d3bbb63976d437b1
Milan <milansonyofficial@gmail.com>
2024-11-13 17:13:24 +0530

    feat: cards added to the live page

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+    return (
+        <>
+            <div className='mx-5 my-5'>
+                <div className='flex justify-evenly'>
+
+                    <div className='px-3 py-3 max-w-[150px] break-words max-h-[auto] card-bg rounded-lg'>
+                        <div className='flex justify-center items-center'>
+                            <div className="w-16 h-16 rounded-full ring-2 ring-white bg-custblue flex justify-center  items-center text-center mb-3">
+                                <p className='text-2xl text-white font-black'>MS</p>
+                            </div>
+                        </div>
+                        <p className='text-custblue text-sm font-Open-Sans font-bold text-center pb-3'>Milan</p>
+                        <hr className='mb-3' />
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Build a project</p>
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Node.js</p>
+                        <marquee behavior="scroll" direction="left"><p className='text-white text-sm font-Open-Sans font-medium'>Sample project</p></marquee>
+                        {/* <p className='text-white text-xs font-Open-Sans font-thin'>milansonyofficial@gmail.com</p> */}
+                    </div>
+                    <div className='px-3 py-3 max-w-[150px] break-words max-h-[auto] card-bg rounded-lg'>
+                        <div className='flex justify-center items-center'>
+                            <div className="w-16 h-16 rounded-full ring-2 ring-white bg-custblue flex justify-center  items-center text-center mb-3">
+                                <p className='text-2xl text-white font-black'>MS</p>
+                            </div>
+                        </div>
+                        <p className='text-custblue text-sm font-Open-Sans font-bold text-center pb-3'>Milan</p>
+                        <hr className='mb-3' />
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Build a project</p>
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Node.js</p>
+                        <marquee behavior="scroll" direction="left"><p className='text-white text-sm font-Open-Sans font-medium'>Sample project</p></marquee>
+                        {/* <p className='text-white text-xs font-Open-Sans font-thin'>milansonyofficial@gmail.com</p> */}
+                    </div>
+                    <div className='px-3 py-3 max-w-[150px] break-words max-h-[auto] card-bg rounded-lg'>
+                        <div className='flex justify-center items-center'>
+                            <div className="w-16 h-16 rounded-full ring-2 ring-white bg-custblue flex justify-center  items-center text-center mb-3">
+                                <p className='text-2xl text-white font-black'>MS</p>
+                            </div>
+                        </div>
+                        <p className='text-custblue text-sm font-Open-Sans font-bold text-center pb-3'>Milan</p>
+                        <hr className='mb-3' />
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Build a project</p>
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Node.js</p>
+                        <marquee behavior="scroll" direction="left"><p className='text-white text-sm font-Open-Sans font-medium'>Sample project</p></marquee>
+                        {/* <p className='text-white text-xs font-Open-Sans font-thin'>milansonyofficial@gmail.com</p> */}
+                    </div>
+                    <div className='px-3 py-3 max-w-[150px] break-words max-h-[auto] card-bg rounded-lg'>
+                        <div className='flex justify-center items-center'>
+                            <div className="w-16 h-16 rounded-full ring-2 ring-white bg-custblue flex justify-center  items-center text-center mb-3">
+                                <p className='text-2xl text-white font-black'>MS</p>
+                            </div>
+                        </div>
+                        <p className='text-custblue text-sm font-Open-Sans font-bold text-center pb-3'>Milan</p>
+                        <hr className='mb-3' />
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Build a project</p>
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Node.js</p>
+                        <marquee behavior="scroll" direction="left"><p className='text-white text-sm font-Open-Sans font-medium'>Sample project</p></marquee>
+                        {/* <p className='text-white text-xs font-Open-Sans font-thin'>milansonyofficial@gmail.com</p> */}
+                    </div>
+                    <div className='px-3 py-3 max-w-[150px] break-words max-h-[auto] card-bg rounded-lg'>
+                        <div className='flex justify-center items-center'>
+                            <div className="w-16 h-16 rounded-full ring-2 ring-white bg-custblue flex justify-center  items-center text-center mb-3">
+                                <p className='text-2xl text-white font-black'>MS</p>
+                            </div>
+                        </div>
+                        <p className='text-custblue text-sm font-Open-Sans font-bold text-center pb-3'>Milan</p>
+                        <hr className='mb-3' />
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Build a project</p>
+                        <p className='text-white text-sm font-Open-Sans font-medium text-center pb-1'>Node.js</p>
+                        <marquee behavior="scroll" direction="left"><p className='text-white text-sm font-Open-Sans font-medium'>Sample project</p></marquee>
+                        {/* <p className='text-white text-xs font-Open-Sans font-thin'>milansonyofficial@gmail.com</p> */}
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
--- src/views/Live/Live.jsx
+++ src/views/Live/Live.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Live.css'
 import Navbar from '../../components/Navbar/Navbar'
+import Card from '../../components/Card/Card'
 
 function Live() {
     return (
         <>
-        <div className='h-screen dynamic-bg'>
-            <Navbar />
-        </div>
+            <div className='h-screen dynamic-bg'>
+                <Navbar />
+                <Card />
+            </div>
         </>
     )
 }
