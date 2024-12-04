68ec55efcbaa87f0851e81760847cd59c3834a0d
Milan <milansonyofficial@gmail.com>
2024-12-04 17:17:36 +0530

    user data logged on console

--- src/views/Live/Live.jsx
+++ src/views/Live/Live.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import './Live.css'
-import Navbar from '../../components/Navbar/Navbar'
-import Card from '../../components/Card/Card'
+import React, { useEffect, useState } from 'react';
+import './Live.css';
+import Navbar from '../../components/Navbar/Navbar';
+import Card from '../../components/Card/Card';
+import axios from 'axios';
 
 function Live() {
+    useEffect(() => {
+        getProfiles();
+    }, []);
+
+    const [data, setData] = useState();
+
+    const getProfiles = async () => {
+        try {
+            const response = await axios.get(import.meta.env.VITE_APP_API_URL + `users`);
+            setData(response.data);
+            console.log(response.data);
+            console.log('hi');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <>
             <div className='h-screen dynamic-bg'>
@@ -11,7 +29,7 @@ function Live() {
                 <Card />
             </div>
         </>
-    )
+    );
 }
 
-export default Live
+export default Live;
